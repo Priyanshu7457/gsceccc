09ac9413ccf1d31ad4aa72c2aefd12e05564db4c
PRIYANSHU SHARMA <131241998+Priyanshu7457@users.noreply.github.com>
2023-11-14 20:01:03 +0530

    Update ccc2.js

--- Combine Test/CCC/ccc2.js
+++ Combine Test/CCC/ccc2.js
@@ -506,10 +506,9 @@ const quizData = [
         correctAnswer: "Private"
     }
 ];
-
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
+        const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
